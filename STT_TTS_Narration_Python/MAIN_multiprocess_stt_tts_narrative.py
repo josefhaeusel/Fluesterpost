9be48cc2ee0602398ba30cda8179452506c4d6c5
Pascal G <psclly@gmail.com>
2023-10-11 14:21:18 +0200

    idk merge didnt work

--- STT_TTS_Narration_Python/MAIN_multiprocess_stt_tts_narrative.py
+++ STT_TTS_Narration_Python/MAIN_multiprocess_stt_tts_narrative.py
@@ -3,6 +3,7 @@ import wavio as wv
 import datetime
 import pathlib
 import os
+import re 
 import glob
 import ssl
 import whisper
@@ -16,6 +17,7 @@ from pythonosc import udp_client
 
 ### SSL Bugfix Whisper SST
 ssl._create_default_https_context = ssl._create_unverified_context
+
 ### Global Variables
 DIR_PATH = pathlib.Path(__file__).parent.resolve()
 RECORDINGS_DIR = os.path.join(f'{DIR_PATH}/recordings/', '*')
@@ -81,14 +83,14 @@ def record_audio():
         sd.wait()
         wv.write(f"{DIR_PATH}/recordings/{filename}.wav", recording, freq, sampwidth=2)
 
-def speech_to_text(first_delete_all_recs = False, delete_recordings_callback = delete_all_recordings):
+def speech_to_text(first_delete_all_recs = True, delete_recordings_callback = delete_all_recordings):
     """
     Transcribes the latest recorded files from record_audio() in ../recordings/... into text, exluding the already transcribes ones.
 
     """
     
     if first_delete_all_recs:
-        last_file = delete_recordings_callback(except_last = True)
+        last_file = delete_recordings_callback(except_last = False)
     else:
         last_file = ""
 
@@ -138,7 +140,10 @@ def speech_to_text(first_delete_all_recs = False, delete_recordings_callback = d
                 transcription_done = True
         
     print("\n\nTRANSCRIPTION DONE:", result.text, "\n\n")
-    return result.text 
+    return result.text
+
+
+    
 
 def text_to_speech(text, mute_mic = True):
 
@@ -232,6 +237,7 @@ def extend_characters(string, characters_to_extend = "aeiouAEIOU", length_result
 
     return extended_string
 
+
 def add_period(string):
 
     """
@@ -246,6 +252,8 @@ def add_period(string):
         return string + '.'
     else:
         return string  
+
+
 
 
 ## Narrative
@@ -264,25 +272,31 @@ def narrative(tts_callback_function, stt_callback_function):
     for i in range(1):
         osc_message("/rec_channel", 1)
         
-<<<<<<< HEAD
         text = "Hello, and welcome, dear visitor, to my totally inconspicuous human voluntary dee. dee. tainment center."
-=======
-        text = "Hello, and welcome, dear visitor, to my totally inconspicuous human voluntary de. de. tainment center. \
-                I will now need to verify your voice object, for I have not spoken to a human entity for vvvvvvvv vvvvvvv two, two, thousand. . . . . two years.\
-                Tell me your p p pain, I mean tell me your name?"
->>>>>>> 2360b7a56e6eea8293da356009307e57c2753d6f
         tts_callback_function(text)
 
+        text = "I will now need to verify your voice object, for I have not spoken to a human entity for vvvvvvvv vvvvvvv two, two, thousand. . . . . two two years."
+        tts_callback_function(text)
+
+        text = "Tell me your pain, I mean. tell me your name?"
+        tts_callback_function(text)
         nameInput = stt_callback_function()
         nameOut1 = extend_characters(nameInput, length_resulting_extension = 4)
         nameOut2 = extend_characters(nameInput, length_resulting_extension = 4, characters_to_extend = "qwrtplkjhgfdszxcbmnv")
-        nameReal =  extend_characters(nameInput, length_resulting_extension = 2)
+        nameReal = extend_characters(nameInput, length_resulting_extension = 2)
 
-        text = f"{nameOut1}. wgwgg hmm. {nameOut2}. ztztztztztztztztztzt. I will just call you {nameReal} from now on."
+        text = f"{nameOut1}. wgwgg hmm. {nameOut2}. ztztztztztztztztztzt. I will just call you {nameReal} from now on. It is a pleasure to kkkkkkkkkl meet, meet you, {nameReal}!"
         tts_callback_function(text)
-        text = f"Pleasure to kkkkkkkkkl meet, meet you {nameReal}!"
+        text = f""
         
         
+
+        
+
+
+
+
+
 
         """name1 = remove_non_letters(stt_callback_function())
         text = f"Alright, {name1}. I will call you {name1} from now on. What did you eat for breakfast, {name1}?"
@@ -322,6 +336,3 @@ if __name__ == '__main__':
     recorder_process.join()
     narrative_process.join()
     clear_cache_process.join()
-
-
-    "C'est quoi?"
